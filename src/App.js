a3102da271ae68b88081cd57184fa4bd4e7ccb16
Rohan Mitra <rohanmitra2420@gmail.com>
2022-09-30 15:23:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React  from 'react';
+import './App.css';
+import {Routes, Route} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Crud from "./crud";
+import Update from "./crud/Update";
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <Routes>
+        <Route exact path ="/" element = {<Crud/>} />
+        <Route exact path ="/update/:id" element = {<Update/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
